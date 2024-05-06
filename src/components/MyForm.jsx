46969bf13bd4e013094d59be98ee8bbeaebc0e0d
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-06 11:59:24 -0300

    terminou sessao

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -2,10 +2,14 @@ import "./MyForm.css"
 
 import { useState } from "react";
 
-const MyForm = () => {
+const MyForm = ({userName, UserEmail}) => {
     // 3 - Gerenciamento de dados
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
+    const [name, setName] = useState(userName);
+    const [email, setEmail] = useState(UserEmail);
+
+    const [bio, setBio] = useState("");
+
+    const [role, setRole] = useState("");
 
     const handleName = (e) => {
 
@@ -17,10 +21,16 @@ const MyForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(name, email);
+        console.log(name, email, bio, role);
 
         //validacao
         //envio
+
+        //7 - limpar a form
+        setName("")
+        setEmail("")
+        setBio("");
+        setRole("");
     }
 
   return (
@@ -30,13 +40,32 @@ const MyForm = () => {
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
+                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}
+                // 6 - controlled input
+                value={name || ""}
+                />
             </div>
             {/* 2 - Label envolvendo input */}
             <label>
                 <span>E-mail:</span>
                 {/* Simplificando manipulação */}
-                <input type="text" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} value={email || ""}/>
+            </label>
+
+            {/* 8 - textarea */}
+            <label>
+                <span>Bio:</span>
+                <textarea name="bii" placeholder="Descrição do usuario" onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+            </label>
+
+            {/* 9 - select */}
+            <label>
+                <span>Função no sistema</span>
+                <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Admin</option>
+                </select>
             </label>
             <input type="submit" value="Enviar"/>
         </form>
